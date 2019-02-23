28074b2edf0df91ed5c142a26fd3d6feb91767f4
JaffaYa <jaffaya@gmail.com>
2019-02-23 12:18:27 +0200

    Зміна главної сторінки

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,13 +2,27 @@
 
 @section('content')
 <div class="container main">
-    <ul class="list-group">
-        <li class="list-group-item"><a href="{{ route('tasks') }}">Зробити</a></li>
-        <li class="list-group-item"><a href="{{ route('toBays') }}">Купити</a></li>
-        <li class="list-group-item"><a href="{{ route('toTravel') }}">Побувати</a></li>
-        <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/films.php">Продивитись</a></li>
-        <li class="list-group-item"><a href="{{ route('toReads') }}">Почитати</a></li>
-        <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/privichka.php">Правило 20 минут</a></li>
-    </ul>
+    <div class="main-ul-wraper">
+        <ul class="list-group main-ul">
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="{{ route('tasks') }}">Зробити</a></li>
+            </div>
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="{{ route('toBays') }}">Купити</a></li>
+            </div>
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="{{ route('toTravel') }}">Побувати</a></li>
+            </div>
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/films.php">Продивитись</a></li>
+            </div>
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="{{ route('toReads') }}">Почитати</a></li>
+            </div>
+            <div class="main-li-wraper">
+                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/privichka.php">Правило 20 минут</a></li>
+            </div>
+        </ul>
+    </div>
 </div>
 @endsection

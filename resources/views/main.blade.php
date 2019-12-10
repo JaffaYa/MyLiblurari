a868eae19532ed1344ad6a0358775f9af83365c7
JaffaYa <jaffaya@gmail.com>
2019-12-11 01:23:42 +0200

    Ізміненіе в головній сторінці

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,22 +5,22 @@
     <div class="main-ul-wraper">
         <ul class="list-group main-ul">
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="{{ route('tasks') }}">Зробити</a></li>
+                <li class="list-group-item"><a href="{{ route('tasks') }}"><span class="text-block">Зробити</span></a></li>
             </div>
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="{{ route('toBays') }}">Купити</a></li>
+                <li class="list-group-item"><a href="{{ route('toBays') }}"><span class="text-block">Купити</span></a></li>
             </div>
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="{{ route('toTravel') }}">Побувати</a></li>
+                <li class="list-group-item"><a href="{{ route('toTravel') }}"><span class="text-block">Побувати</span></a></li>
             </div>
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/films.php">Продивитись</a></li>
+                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/films.php"><span class="text-block">Продивитись</span></a></li>
             </div>
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="{{ route('toReads') }}">Почитати</a></li>
+                <li class="list-group-item"><a href="{{ route('toReads') }}"><span class="text-block">Почитати</span></a></li>
             </div>
             <div class="main-li-wraper">
-                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/privichka.php">Правило 20 минут</a></li>
+                <li class="list-group-item"><a href="http://s2.plopan.com/MyBase/privichka.php"><span class="text-block">Правило 20 минут</span></a></li>
             </div>
         </ul>
     </div>
